5c17c6fb3609ec42fc24db1245328154cb42c19f
mikasa <1244456115@qq.com>
2025-05-14 09:11:24 +0800

    neoforge-mc1.20.(3/4)

---
+++ src/main/java/de/dafuqs/paginatedadvancements/mixin/MinecraftClientMixin.java
@@ -0,0 +1,35 @@
+package de.dafuqs.paginatedadvancements.mixin;
+
+import de.dafuqs.paginatedadvancements.client.PaginatedAdvancementScreen;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.advancements.AdvancementsScreen;
+import net.minecraft.client.player.LocalPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import javax.annotation.Nullable;
+
+@Mixin(Minecraft.class)
+public abstract class MinecraftClientMixin {
+
+
+    @Shadow
+    @Nullable
+    public LocalPlayer player;
+
+    /**
+     * Redirect all calls to the vanilla advancement screen to out custom one
+     * Other screens that extend AdvancementScreen will not be touched
+     */
+    @ModifyVariable(method = "setScreen", at = @At("HEAD"), argsOnly = true)
+    private Screen paginatedAdvancements$modifyAdvancementsScreen(Screen screen) {
+        if (this.player != null && screen != null && AdvancementsScreen.class == screen.getClass()) {
+            return new PaginatedAdvancementScreen(this.player.connection.getAdvancements());
+        } else {
+            return screen;
+        }
+    }
+}
